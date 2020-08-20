0ac83ef9f8943eeecd70cbe7a75d23ff1dd559a6
Heverton Luiz <hevertonluizs@gmail.com>
2020-08-20 16:52:41 -0300

    Regra de fim de jogo

--- script.js
+++ script.js
@@ -59,6 +59,16 @@ function iniciarJogo() {
     if (snake[0].x < 0 && direccion == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direccion == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direccion == "up") snake[0].y = 16 * box;
+
+    // Game Over
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert("Game Over!")
+        }
+
+    }
     
 
     criarBG();
